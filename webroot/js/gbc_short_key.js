45f5ae3f45e776a590530e780e820b37d3224280
gondoh <gondoh@gufii.net>
2016-03-08 15:35:14 +0900

    保存制限を追加

--- webroot/js/gbc_short_key.js
+++ webroot/js/gbc_short_key.js
@@ -1,10 +1,13 @@
 ;(function($){
 	$(function(){
 		var $contentElm  = $("#ContentsBody");
+		var toolbar = null;
 		
 		// 保存
 		var $form = $contentElm.find("form:first");
-		if ($form.length) {
+		// 全部で動くと危ないのでadd / editのみ動作
+		if ($form.length && /(\/add$|\/edit\/)/.test(location.href)) {
+			toolbar += "Command + Enter<保存> \\n";
 			$(window).keydown( function ( event ){
 				if( event.metaKey === true && event.which === 13 ){
 					$form.submit();
@@ -19,6 +22,7 @@
 		var soreppoiUrl = null;
 		// 一番最後の
 		if (soreppoiUrlMatches.length) {
+			toolbar += "Command + C<新規>";
 			soreppoiUrlMatches = soreppoiUrlMatches[soreppoiUrlMatches.length-1].match(/href=("(.*)"|'(.*)')/);
 			// 3番目
 			soreppoiUrl = soreppoiUrlMatches[2];
@@ -31,6 +35,6 @@
 		}
 		
 		// ツールバーにショートカットを表示する
-		$("#ToolMenu ul").append("<li><a onclick='alert(\"Command + Enter<保存> \\nCommand + C<新規>\");' style='color:#FAA;'>GbcShotKey有効</a></li>");
+		$("#ToolMenu ul").append("<li><a onclick='alert(\"" + toolbar + "\");' style='color:#FAA;'>GbcShotKey有効</a></li>");
 	});
 })(jQuery);
